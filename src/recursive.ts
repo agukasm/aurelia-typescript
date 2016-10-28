59525748c1a3ec64ec8f3d9d9b1c4b97e85a00aa
Mati Agukas <mati.agukas@cgi.com>
2016-10-29 01:17:44 +0300

    Recursive components

---
+++ src/recursive.ts
@@ -0,0 +1,10 @@
+﻿import {bindable} from "aurelia-framework";
+
+export class Recursive {
+  @bindable firstName: string = "";
+  @bindable lastName: string = "";
+
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
